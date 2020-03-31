306386540c1f01a10c6c64a21ccd437c548d5bc6
yassine <yassine.lf.99@gmail.com>
2020-03-31 02:23:25 +0100

    new changes

--- api/date.py
+++ api/date.py
@@ -1,11 +1,9 @@
-from http.server import BaseHTTPRequestHandler
-from datetime import datetime
+from flask import Flask
 
-class handler(BaseHTTPRequestHandler):
 
-  def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
-    self.end_headers()
-    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
-    return
+app = Flask(__name__)
+
+
+@app.route(r'/')
+def index():
+    return "hello world"
